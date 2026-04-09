fe296608720793380132ea707de91e31156ecd9d
I0635 <nayoung0352@gmail.com>
2026-04-09 22:14:15 +0900

    Add files via upload

---
+++ stack.h
@@ -0,0 +1,18 @@
+//
+// Created by nayoung on 26. 4. 7..
+//
+
+#ifndef STACK_STACK_H
+#define STACK_STACK_H
+#define MAX 100
+typedef struct {
+    int data[MAX];
+    int top;
+}Stack;
+void initStack(Stack *s);
+int isEmpty(Stack *s);
+void push(Stack *s, int value);
+int pop(Stack *s);
+int peek(Stack *s);
+void display(Stack *s);
+#endif //STACK_STACK_H
